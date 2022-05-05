c9e936bef41274b489262e18b9ea704e282d043a
mermer vivo <mincoln419@naver.com>
2022-05-05 09:41:45 +0900

    comparator
    comparator

--- Streaming-Excercise/src/com/mermer/ch1/Chapter1Section5.java
+++ Streaming-Excercise/src/com/mermer/ch1/Chapter1Section5.java
@@ -2,8 +2,12 @@ package com.mermer.ch1;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 import java.util.function.Predicate;
+
+import com.mermer.ch10.User;
 
 public class Chapter1Section5 {
 	
@@ -11,23 +15,30 @@ public class Chapter1Section5 {
 	
 	public static void main(String[] args) {
 		
-		Predicate<Integer> isPositive = x -> x > 0;
-		Predicate<Integer> isNonPositive = x -> x < 0;
+		List<User> list = new ArrayList<>();
+		User user1= User.builder(201, "alice").build();
+		User user2= User.builder(102, "bear").build();
+		User user3= User.builder(101, "chalie").build();
+		list.add(user1);
+		list.add(user2);
+		list.add(user3);
+		Comparator<User> nameComparator = (x, y) -> x.getName().compareTo(y.getName());
 		
-		List<Integer> inputs = Arrays.asList(10,-1,20,-30,0,20,12);
-		List<Integer> positive = filter(inputs, isPositive);
-		System.out.println(positive);
-		List<Integer> nonPositive = filter(inputs, isNonPositive);
-		System.out.println(nonPositive);
+		Collections.sort(list, nameComparator);
+		System.out.println(list);
+		
+		Comparator<User> idComparator = (x, y) -> x.getId() - y.getId();
+		
+		Collections.sort(list, idComparator);
+		System.out.println(list);
+		
+		
 	}
 	
-	public static <T> List<T> filter(List<T> inputs, Predicate<T> condition){
+	public static <T> List<T> sort(List<T> inputs, Predicate<T> condition){
 		
-		List<T> output = new ArrayList<>();
-		for(T input : inputs) {
-			if(condition.test(input))output.add(input);
-		}
-		return output;
+		
+		return null;
 	}
 	
 
