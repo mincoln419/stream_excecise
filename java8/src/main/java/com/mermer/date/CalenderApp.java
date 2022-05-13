71113510f68ce466982d098efbd0c9916604a95b
mermer <mincoln419@naver.com>
2022-05-13 09:00:37 +0900

    기존 java calender 문제점
    기존 java calender 문제점

--- java8/src/main/java/com/mermer/date/CalenderApp.java
+++ java8/src/main/java/com/mermer/date/CalenderApp.java
@@ -8,7 +8,11 @@ public class CalenderApp {
 	
 	public static void main(String[] args) {
 		
-		Calendar calender = new GregorianCalendar(1987, 4, 19);
+		Calendar calender = new GregorianCalendar(1987, 4, 19); //month가 0부터 시작되면서 4 -> 5월임..
 		System.out.println(calender.getTime());
+		
+		//joda-time 프로젝트의 계기가 됨.. -> java 표준이 됨: date-time api.. -> Localdatetime, localdate
+		
+		
 	}
 }
