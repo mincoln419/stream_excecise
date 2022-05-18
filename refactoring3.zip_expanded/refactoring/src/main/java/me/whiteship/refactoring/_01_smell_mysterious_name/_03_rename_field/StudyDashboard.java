2c4a8f07c626e2258783ca55d083cac548ae9ade
mermer <mincoln419@naver.com>
2022-05-19 08:18:05 +0900

    필드이름 바꾸기, 함수추출하기
    필드이름 바꾸기, 함수추출하기

--- refactoring3.zip_expanded/refactoring/src/main/java/me/whiteship/refactoring/_01_smell_mysterious_name/_03_rename_field/StudyDashboard.java
+++ refactoring3.zip_expanded/refactoring/src/main/java/me/whiteship/refactoring/_01_smell_mysterious_name/_03_rename_field/StudyDashboard.java
@@ -12,38 +12,34 @@ import java.util.Set;
 
 public class StudyDashboard {
 
-    private Set<String> usernames = new HashSet<>();
+	private Set<StudyReview> studyReviewers = new HashSet<>();
 
-    private Set<String> reviews = new HashSet<>();
+	/**
+	 * 스터디 리뷰 이슈에 작성되어 있는 리뷰어 목록과 리뷰를 읽어옵니다.
+	 * 
+	 * @throws IOException
+	 */
+	private void loadReviews() throws IOException {
+		GitHub gitHub = GitHub.connect();
+		GHRepository repository = gitHub.getRepository("whiteship/live-study");
+		GHIssue issue = repository.getIssue(30);
 
-    /**
-     * 스터디 리뷰 이슈에 작성되어 있는 리뷰어 목록과 리뷰를 읽어옵니다.
-     * @throws IOException
-     */
-    private void loadReviews() throws IOException {
-        GitHub gitHub = GitHub.connect();
-        GHRepository repository = gitHub.getRepository("whiteship/live-study");
-        GHIssue issue = repository.getIssue(30);
+		List<GHIssueComment> reviews = issue.getComments();
+		for (GHIssueComment review : reviews) {
+			studyReviewers.add(new StudyReview(review.getUserName(), review.getBody()));
+		}
+	}
 
-        List<GHIssueComment> reviews = issue.getComments();
-        for (GHIssueComment review : reviews) {
-            usernames.add(review.getUserName());
-            this.reviews.add(review.getBody());
-        }
-    }
+	
 
-    public Set<String> getUsernames() {
-        return usernames;
-    }
+	public Set<StudyReview> getStudyReviewers() {
+		return studyReviewers;
+	}
 
-    public Set<String> getReviews() {
-        return reviews;
-    }
 
-    public static void main(String[] args) throws IOException {
-        StudyDashboard studyDashboard = new StudyDashboard();
-        studyDashboard.loadReviews();
-        studyDashboard.getUsernames().forEach(System.out::println);
-        studyDashboard.getReviews().forEach(System.out::println);
-    }
+	public static void main(String[] args) throws IOException {
+		StudyDashboard studyDashboard = new StudyDashboard();
+		studyDashboard.loadReviews();
+		studyDashboard.getStudyReviewers().forEach(System.out::println);
+	}
 }
--- refactoring3.zip_expanded/refactoring/src/main/java/me/whiteship/refactoring/_02_duplicated_code/_04_extract_function/StudyDashboard.java
+++ refactoring3.zip_expanded/refactoring/src/main/java/me/whiteship/refactoring/_02_duplicated_code/_04_extract_function/StudyDashboard.java
@@ -11,32 +11,32 @@ import java.util.Set;
 public class StudyDashboard {
 
     private void printParticipants(int eventId) throws IOException {
-        // Get github issue to check homework
-        GitHub gitHub = GitHub.connect();
+        GHIssue issue = getGhIssue(eventId);
+        Set<String> participants = getUsername(issue);
+        print(participants);
+    }
+
+	private Set<String> getUsername(GHIssue issue) throws IOException {
+		Set<String> usernames = new HashSet<>();
+        issue.getComments().forEach(c -> usernames.add(c.getUserName()));
+		return usernames;
+	}
+
+	private GHIssue getGhIssue(int eventId) throws IOException {
+		GitHub gitHub = GitHub.connect();
         GHRepository repository = gitHub.getRepository("whiteship/live-study");
         GHIssue issue = repository.getIssue(eventId);
-
-        // Get participants
-        Set<String> participants = new HashSet<>();
-        issue.getComments().forEach(c -> participants.add(c.getUserName()));
-
-        // Print participants
-        participants.forEach(System.out::println);
-    }
+		return issue;
+	}
 
     private void printReviewers() throws IOException {
-        // Get github issue to check reviews
-        GitHub gitHub = GitHub.connect();
-        GHRepository repository = gitHub.getRepository("whiteship/live-study");
-        GHIssue issue = repository.getIssue(30);
-
-        // Get reviewers
-        Set<String> reviewers = new HashSet<>();
-        issue.getComments().forEach(c -> reviewers.add(c.getUserName()));
-
-        // Print reviewers
-        reviewers.forEach(System.out::println);
+        Set<String> reviewers = getUsername(getGhIssue(30));
+        print(reviewers);
     }
+
+	private void print(Set<String> reviewers) {
+		reviewers.forEach(System.out::println);
+	}
 
     public static void main(String[] args) throws IOException {
         StudyDashboard studyDashboard = new StudyDashboard();
