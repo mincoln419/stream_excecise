2d8beadfb88b3e40413e1be6d226230ead18892c
mermer vivo <mincoln419@naver.com>
2022-05-21 08:41:55 +0900

    decomposed extract
    decomposed extract

--- refactoring3.zip_expanded/refactoring/src/main/java/me/whiteship/refactoring/_03_long_function/_11_decompose_conditional/StudyDashboard.java
+++ refactoring3.zip_expanded/refactoring/src/main/java/me/whiteship/refactoring/_03_long_function/_11_decompose_conditional/StudyDashboard.java
@@ -14,63 +14,72 @@ import java.util.concurrent.Executors;
 
 public class StudyDashboard {
 
-    private final int totalNumberOfEvents;
+	private final int totalNumberOfEvents;
 
-    public StudyDashboard(int totalNumberOfEvents) {
-        this.totalNumberOfEvents = totalNumberOfEvents;
-    }
+	public StudyDashboard(int totalNumberOfEvents) {
+		this.totalNumberOfEvents = totalNumberOfEvents;
+	}
 
-    public static void main(String[] args) throws IOException, InterruptedException {
-        StudyDashboard studyDashboard = new StudyDashboard(15);
-        studyDashboard.print();
-    }
+	public static void main(String[] args) throws IOException, InterruptedException {
+		StudyDashboard studyDashboard = new StudyDashboard(15);
+		studyDashboard.print();
+	}
 
-    private void print() throws IOException, InterruptedException {
-        GitHub gitHub = GitHub.connect();
-        GHRepository repository = gitHub.getRepository("whiteship/live-study");
-        List<Participant> participants = new CopyOnWriteArrayList<>();
+	private void print() throws IOException, InterruptedException {
+		GitHub gitHub = GitHub.connect();
+		GHRepository repository = gitHub.getRepository("whiteship/live-study");
+		List<Participant> participants = new CopyOnWriteArrayList<>();
 
-        ExecutorService service = Executors.newFixedThreadPool(8);
-        CountDownLatch latch = new CountDownLatch(totalNumberOfEvents);
+		ExecutorService service = Executors.newFixedThreadPool(8);
+		CountDownLatch latch = new CountDownLatch(totalNumberOfEvents);
 
-        for (int index = 1 ; index <= totalNumberOfEvents ; index++) {
-            int eventId = index;
-            service.execute(new Runnable() {
-                @Override
-                public void run() {
-                    try {
-                        GHIssue issue = repository.getIssue(eventId);
-                        List<GHIssueComment> comments = issue.getComments();
+		for (int index = 1; index <= totalNumberOfEvents; index++) {
+			int eventId = index;
+			service.execute(new Runnable() {
+				@Override
+				public void run() {
+					try {
+						GHIssue issue = repository.getIssue(eventId);
+						List<GHIssueComment> comments = issue.getComments();
 
-                        for (GHIssueComment comment : comments) {
-                            Participant participant = findParticipant(comment.getUserName(), participants);
-                            participant.setHomeworkDone(eventId);
-                        }
+						for (GHIssueComment comment : comments) {
+							Participant participant = findParticipant(comment.getUserName(), participants);
+							participant.setHomeworkDone(eventId);
+						}
 
-                        latch.countDown();
-                    } catch (IOException e) {
-                        throw new IllegalArgumentException(e);
-                    }
-                }
-            });
-        }
+						latch.countDown();
+					} catch (IOException e) {
+						throw new IllegalArgumentException(e);
+					}
+				}
+			});
+		}
 
-        latch.await();
-        service.shutdown();
+		latch.await();
+		service.shutdown();
 
-        new StudyPrinter(this.totalNumberOfEvents, participants).execute();
-    }
+		new StudyPrinter(this.totalNumberOfEvents, participants).execute();
+	}
 
-    private Participant findParticipant(String username, List<Participant> participants) {
-        Participant participant;
-        if (participants.stream().noneMatch(p -> p.username().equals(username))) {
-            participant = new Participant(username);
-            participants.add(participant);
-        } else {
-            participant = participants.stream().filter(p -> p.username().equals(username)).findFirst().orElseThrow();
-        }
+	private Participant findParticipant(String username, List<Participant> participants) {
+		return isNewParticipant(username, participants) ? 
+				  createNewParticipant(username, participants)
+				: findExistingParticipant(username, participants);
+	}
 
-        return participant;
-    }
+	private Participant findExistingParticipant(String username, List<Participant> participants) {
+		return participants.stream().filter(p -> p.username().equals(username)).findFirst().orElseThrow();
+	}
+
+	private Participant createNewParticipant(String username, List<Participant> participants) {
+		Participant participant;
+		participant = new Participant(username);
+		participants.add(participant);
+		return participant;
+	}
+
+	private boolean isNewParticipant(String username, List<Participant> participants) {
+		return participants.stream().noneMatch(p -> p.username().equals(username));
+	}
 
 }
