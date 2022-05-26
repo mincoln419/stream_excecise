22acb106e76742992cafba2068c96d25c22640c4
mermer <mincoln419@naver.com>
2022-05-27 07:30:58 +0900

    계층 합치기
    계층 합치기

--- refactoring3.zip_expanded/refactoring/src/main/java/me/whiteship/refactoring/_14_lazy_elements/_34_collapse_hierarchy/CourtReservation.java
+++ refactoring3.zip_expanded/refactoring/src/main/java/me/whiteship/refactoring/_14_lazy_elements/_34_collapse_hierarchy/CourtReservation.java
@@ -1,7 +1,27 @@
 package me.whiteship.refactoring._14_lazy_elements._34_collapse_hierarchy;
 
+import java.time.LocalDateTime;
+
 public class CourtReservation extends Reservation {
 
     private String courtNumber;
+	private LocalDateTime startDateTime;
+	private LocalDateTime endDateTime;
+
+	public LocalDateTime getStartDateTime() {
+		return startDateTime;
+	}
+
+	public void setStartDateTime(LocalDateTime startDateTime) {
+		this.startDateTime = startDateTime;
+	}
+
+	public LocalDateTime getEndDateTime() {
+		return endDateTime;
+	}
+
+	public void setEndDateTime(LocalDateTime endDateTime) {
+		this.endDateTime = endDateTime;
+	}
 
 }
--- refactoring3.zip_expanded/refactoring/src/main/java/me/whiteship/refactoring/_14_lazy_elements/_34_collapse_hierarchy/Reservation.java
+++ refactoring3.zip_expanded/refactoring/src/main/java/me/whiteship/refactoring/_14_lazy_elements/_34_collapse_hierarchy/Reservation.java
@@ -1,18 +1,31 @@
 package me.whiteship.refactoring._14_lazy_elements._34_collapse_hierarchy;
 
-import java.time.LocalDateTime;
 import java.util.List;
 
 public class Reservation {
 
-    private LocalDateTime startDateTime;
+	private List<String> members;
+	private String owner;
+	private boolean paid;
+	public List<String> getMembers() {
+		return members;
+	}
+	public void setMembers(List<String> members) {
+		this.members = members;
+	}
+	public String getOwner() {
+		return owner;
+	}
+	public void setOwner(String owner) {
+		this.owner = owner;
+	}
+	public boolean isPaid() {
+		return paid;
+	}
+	public void setPaid(boolean paid) {
+		this.paid = paid;
+	}
 
-    private LocalDateTime endDateTime;
-
-    private List<String> members;
-
-    private String owner;
-
-    private boolean paid;
-
+	
+	
 }
