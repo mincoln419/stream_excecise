55a537afd016cbf49967e908770867d68916c617
mermer vivo <mincoln419@naver.com>
2022-05-28 06:59:05 +0900

    combine method
    combilne method

--- refactoring3.zip_expanded/refactoring/src/main/java/me/whiteship/refactoring/_05_global_data/_01_before/Thermostats.java
+++ refactoring3.zip_expanded/refactoring/src/main/java/me/whiteship/refactoring/_05_global_data/_01_before/Thermostats.java
@@ -2,14 +2,44 @@ package me.whiteship.refactoring._05_global_data._01_before;
 
 public class Thermostats {
 
-    public static Integer targetTemperature = 70;
+    private static Integer targetTemperature = 70;//상수값을 글로벌에 두는 것은 final 이 필요한 경우만...
 
-    public static Boolean heating = true;
+    private static Boolean heating = true;
 
-    public static Boolean cooling = false;
+    private static Boolean cooling = false;
 
-    public static Boolean fahrenheit = true;
+    private static Boolean fahrenheit = true;
 
+	public static Integer getTargetTemperature() {
+		return targetTemperature;
+	}
 
+	public static void setTargetTemperature(Integer targetTemperature) {
+		Thermostats.targetTemperature = targetTemperature;
+	}
+
+	public static Boolean getHeating() {
+		return heating;
+	}
+
+	public static void setHeating(Boolean heating) {
+		Thermostats.heating = heating;
+	}
+
+	public static Boolean getCooling() {
+		return cooling;
+	}
+
+	public static void setCooling(Boolean cooling) {
+		Thermostats.cooling = cooling;
+	}
+
+	public static Boolean getFahrenheit() {
+		return fahrenheit;
+	}
+
+	public static void setFahrenheit(Boolean fahrenheit) {
+		Thermostats.fahrenheit = fahrenheit;
+	}
 
 }
