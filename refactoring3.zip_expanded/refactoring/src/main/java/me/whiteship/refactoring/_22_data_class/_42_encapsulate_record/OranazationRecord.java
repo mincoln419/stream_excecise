08b19f26839f984cd0ad8603573feec1248a6d47
mermer vivo <mincoln419@naver.com>
2022-05-28 16:07:01 +0900

    데이터 클래스
    encapsulation record

---
+++ refactoring3.zip_expanded/refactoring/src/main/java/me/whiteship/refactoring/_22_data_class/_42_encapsulate_record/OranazationRecord.java
@@ -0,0 +1,5 @@
+package me.whiteship.refactoring._22_data_class._42_encapsulate_record;
+
+public record OranazationRecord(String name) {
+
+}
---
+++ refactoring3.zip_expanded/refactoring/src/test/java/me/whiteship/refactoring/_22_data_class/_42_encapsulate_record/OrganizationTest.java
@@ -0,0 +1,13 @@
+package me.whiteship.refactoring._22_data_class._42_encapsulate_record;
+
+import org.junit.jupiter.api.Test;
+
+class OrganizationTest {
+
+	@Test
+	void test() {
+		Organization organization = new Organization();
+		System.out.println(organization.name);
+	}
+
+}
