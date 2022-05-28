39bdfa04090862aba345f1330dbe2f3f61d1d3c9
mermer vivo <mincoln419@naver.com>
2022-05-28 16:31:55 +0900

    assertion 추가하기
    assertion 추가하기

---
+++ refactoring3.zip_expanded/refactoring/src/test/java/me/whiteship/refactoring/_24_comments/_43_introduce_assertion/CustomerTest.java
@@ -0,0 +1,14 @@
+package me.whiteship.refactoring._24_comments._43_introduce_assertion;
+
+import org.junit.jupiter.api.Test;
+
+class CustomerTest {
+
+	@Test
+	void test() {
+		Customer customer = new Customer();
+		customer.setDiscountRate(-1.0);
+	
+	}
+
+}
