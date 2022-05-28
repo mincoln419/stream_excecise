e8a95667d0da72f5c5f2d0a2ace2ff0b410555d2
mermer vivo <mincoln419@naver.com>
2022-05-28 15:58:57 +0900

    서로 다른 인터페이스의 대안 클래스 처리
    서로 다른 인터페이스의 대안 클래스 처리

--- refactoring3.zip_expanded/refactoring/src/main/java/me/whiteship/refactoring/_21_alternative_classes_with_different_interfaces/OrderAlerts.java
+++ refactoring3.zip_expanded/refactoring/src/main/java/me/whiteship/refactoring/_21_alternative_classes_with_different_interfaces/OrderAlerts.java
@@ -2,12 +2,11 @@ package me.whiteship.refactoring._21_alternative_classes_with_different_interfac
 
 public class OrderAlerts {
 
-    private AlertService alertService;
-    
-    public void alertShipped(Order order) {
-        AlertMessage alertMessage = new AlertMessage();
-        alertMessage.setMessage(order.toString() + " is shipped");
-        alertMessage.setFor(order.getEmail());
-        alertService.add(alertMessage);
-    }
+	private NotificationService notificationService;
+
+	public void alertShipped(Order order) {
+		Notification notification = Notification.newNotification(order.toString() + " is shipped")
+				.receiver(order.getEmail());
+		notificationService.sendNotification(notification);
+	}
 }
