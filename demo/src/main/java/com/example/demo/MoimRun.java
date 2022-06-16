f8bfed2eecb7986dbc79c915d54bfbf26ed16434
mermer <mincoln419@naver.com>
2022-06-17 07:58:41 +0900

    소스 커버리지 수행
    소스 커버리지 수행

---
+++ demo/src/main/java/com/example/demo/MoimRun.java
@@ -0,0 +1,12 @@
+package com.example.demo;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoimRun {
+
+	public static void main(String[] args) {
+		
+	}
+
+}
