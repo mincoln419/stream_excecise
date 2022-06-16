1cb9322ef7ada2d667a86ffd179f1644fbcffdd3
mermer <mincoln419@naver.com>
2022-06-17 08:24:39 +0900

    Byte 조작 (byte buddy) 입문
    Byte 조작 (byte buddy) 입문

---
+++ demo/src/main/java/com/example/demo/Masulsa.java
@@ -0,0 +1,25 @@
+package com.example.demo;
+
+import net.bytebuddy.ByteBuddy;
+import net.bytebuddy.implementation.FixedValue;
+
+import static net.bytebuddy.matcher.ElementMatchers.*;
+
+import java.io.File;
+import java.io.IOException;
+
+public class Masulsa {
+
+	public static void main(String[] args) {
+		try {
+			new ByteBuddy().redefine(Moja.class)
+			.method(named("pullOut")).intercept(FixedValue.value("Rabbit!"))
+			.make().saveIn(new File("C://stream_excecise/demo/target/classes/com/example/demo/"));
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		System.out.println(new Moja().pullOut());
+	}
+	
+}
+
