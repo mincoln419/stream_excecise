93b8c60b93a1a1973dde39fdcb705c57a8e6c6d0
mermer vivo <mincoln419@naver.com>
2022-06-19 16:51:46 +0900

    바이트 코드 정의

---
+++ code-manipulation/src/main/java/com/mermer/BookRepository.java
@@ -0,0 +1,8 @@
+package com.mermer;
+
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class BookRepository {
+
+}
---
+++ code-manipulation/src/main/java/com/mermer/MyConfig.java
@@ -0,0 +1,8 @@
+package com.mermer;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public interface MyConfig {
+
+}
