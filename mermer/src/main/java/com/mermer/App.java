2e59d2a356ec6746bb3355033815a792e569d09d
mermer <mincoln419@naver.com>
2022-06-20 08:01:53 +0900

    Auto stash before merge of "main" and "origin/main"

---
+++ mermer/src/main/java/com/mermer/App.java
@@ -0,0 +1,17 @@
+package com.mermer;
+
+public class App {
+	
+    public static void main(String[] args){
+    	new App().start();
+    }
+    
+    public App() {
+    	
+	}
+    
+    public void start() {
+
+	}
+    
+}
---
+++ mermer/src/test/java/com/mermer/AppTest.java
@@ -0,0 +1,14 @@
+package com.mermer;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.junit.jupiter.api.Test;
+
+class AppTest {
+
+	@Test
+	void test() {
+
+	}
+
+}
+
