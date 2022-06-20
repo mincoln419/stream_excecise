995c22a5280e8c172ebe520778322bdedd2354d6
mermer <mincoln419@naver.com>
2022-06-21 08:41:07 +0900

    다이나믹 프록시
    다이나믹 프록시

--- demo/src/main/java/com/example/demo/di/BookService.java
+++ demo/src/main/java/com/example/demo/di/BookService.java
@@ -1,8 +1,17 @@
 package com.example.demo.di;
 
+import com.example.demo.reflection.Book;
+
 public class BookService {
 
+	
+	
 	@Inject
 	BookRepository bookRepository;
+	
+	public void rent(Book book) {
+		System.out.println("aaaaa");
+		System.out.println("bbbbb");
+	}
 	
 }
---
+++ demo/src/test/java/com/example/demo/di/ProxyTest.java
@@ -0,0 +1,29 @@
+package com.example.demo.di;
+
+import java.lang.reflect.InvocationHandler;
+import java.lang.reflect.Method;
+import java.lang.reflect.Proxy;
+
+import org.junit.jupiter.api.Test;
+
+public class ProxyTest {
+
+	BookService bookService = (BookService) Proxy.newProxyInstance(BookService.class.getClassLoader(), new Class[] {
+			BookService.class
+	}, new InvocationHandler() {
+		BookService bookService = new BookService();
+		@Override
+		public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
+			
+			System.out.println("aaaaaaaaaaaaaaaaaaa");
+			
+			return method.invoke(bookService, args);
+		}
+	});
+
+	@Test
+	public void rent() {
+
+	}
+
+}
